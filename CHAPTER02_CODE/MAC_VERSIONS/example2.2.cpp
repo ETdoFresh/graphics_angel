43915913e6cda4d3bdf93731d76f75a0bf9f0d20
Chris Summa <csumma@uno.edu>
2012-09-05 10:09:38 -0500

    generalized example 2.1 and 2.2 in MAC_VERSIONS - should be cross-platform now

--- CHAPTER02_CODE/MAC_VERSIONS/example2.2.cpp
+++ CHAPTER02_CODE/MAC_VERSIONS/example2.2.cpp
@@ -54,8 +54,13 @@ init( void )
 
     // Create a vertex array object
     GLuint vao;
+#ifdef __APPLE__
     glGenVertexArraysAPPLE( 1, &vao );
     glBindVertexArrayAPPLE( vao );
+#else
+    glGenVertexArrays( 1, &vao );
+    glBindVertexArray( vao );
+#endif
 
     // Create and initialize a buffer object
     GLuint buffer;
@@ -64,7 +69,17 @@ init( void )
     glBufferData( GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW );
 
     // Load shaders and use the resulting shader program
-    GLuint program = InitShader( "vshader22.glsl", "fshader22.glsl" );
+	std::string evname = "ANGELDIR";
+	std::string path = getEnvironmentVariable(evname);
+	path += "/shaders/";
+#ifdef __APPLE__
+	path += "/MAC_VERSIONS/";
+#else
+	path += "/WINDOWS_VERSIONS/";
+#endif
+	std::string vshader = path + "vshader22.glsl";
+	std::string fshader = path + "fshader22.glsl";
+    GLuint program = InitShader( vshader.c_str(), fshader.c_str() );
     glUseProgram( program );
 
     // Initialize the vertex position attribute from the vertex shader    
@@ -106,7 +121,17 @@ main( int argc, char **argv )
     glutInit( &argc, argv );
     glutInitDisplayMode( GLUT_RGBA );
     glutInitWindowSize( 512, 512 );
+#ifndef __APPLE__
+    glutInitContextVersion( 3, 2 );
+    glutInitContextProfile( GLUT_CORE_PROFILE );
+#endif
     glutCreateWindow( "Simple GLSL example" );
+#ifdef GLEW_EXPERIMENTAL
+    glewExperimental = GL_TRUE;
+#endif
+#ifndef __APPLE__
+    glewInit();
+#endif
 
     init();
 
