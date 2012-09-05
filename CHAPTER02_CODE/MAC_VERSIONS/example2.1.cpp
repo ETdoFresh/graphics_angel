e42c8616276dfca73036c2646304e7006bc81493
Chris Summa <csumma@uno.edu>
2012-09-05 15:24:40 -0500

    generalized CHAPTER08 code

--- CHAPTER02_CODE/MAC_VERSIONS/example2.1.cpp
+++ CHAPTER02_CODE/MAC_VERSIONS/example2.1.cpp
@@ -30,13 +30,13 @@ init( void )
     }
 
     // Create a vertex array object
-    GLuint vao[1];
+    GLuint vao;
 #ifdef __APPLE__
-    glGenVertexArraysAPPLE( 1, vao );
-    glBindVertexArrayAPPLE( vao[0] );
+    glGenVertexArraysAPPLE( 1, &vao );
+    glBindVertexArrayAPPLE( vao );
 #else
-    glGenVertexArrays( 1, vao );
-    glBindVertexArray( vao[0] );
+    glGenVertexArrays( 1, &vao );
+    glBindVertexArray( vao );
 #endif
 
     // Create and initialize a buffer object
